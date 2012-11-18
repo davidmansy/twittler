ae51bbcbd7d388d627b4d931c4c3e3d9a0759ee5
Shawn Drost <sdrost@gmail.com>
2012-11-17 18:12:32 -0800

    get twitter_ui started

--- twitter_ui.js
+++ twitter_ui.js
@@ -1,1 +1,10 @@
-/*code goes here*/
+window.draw = function(){
+  var body = $("body");
+  body.html("")
+  for(var i = window.streams.home.length - 1; i >= 0; i-- ){
+    tweet = window.streams.home[i]
+    console.log(tweet)
+    body.append(tweet.user + ": " + tweet.message + "<br>");
+  }
+}
+setTimeout(draw, 2000)
