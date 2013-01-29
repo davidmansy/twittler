fedd60658dac7bda9be8ecd66b3b4b94147546fe
Marcus Phillips <github.com@marcusphillips.com>
2013-01-29 15:18:46 -0800

    uniformity of language patterns

--- twitter_data.js
+++ twitter_data.js
@@ -1,45 +1,43 @@
 // set up data structures
-window.streams = {
-  users: {
-    shawndrost: [],
-    sharksforcheap: [],
-    mracus: [],
-    douglascalhoun: []
-  },
-  home: []
-};
-window.users = Object.keys(streams.users);
+window['streams'] = {};
+streams['home'] = {};
+streams['users'] = {};
+streams['users']['shawndrost'] = [];
+streams['users']['sharksforcheap'] = [];
+streams['users']['mracus'] = [];
+streams['users']['douglascalhoun'] = [];
+window['users'] = Object['keys'](streams['users']);
 
 // utility function for adding tweets to our data structures
 var add_tweet = function(new_tweet){
-  // console.log(new_tweet)
-  streams.users[new_tweet.user].push(new_tweet);
-  streams.home.push(new_tweet);
+  var username = new_tweet['user'];
+  streams['users'][username]['push'](new_tweet);
+  streams['home']['push'](new_tweet);
 };
 
 // utility function
-var random = function(array){
-  var random_index = Math.floor(Math.random() * array.length);
+var randomElement = function(array){
+  var random_index = Math['floor'](Math['random']() * array['length']);
   return array[random_index];
-}
+};
 
 // random tweet generator
-var verbs = ["punched", "considered", "laughed at", "selected", "marginalized", "drank", "punk'd"];
-var objects = ["my", "your", "the"];
-var nouns = ["cat", "elbow", "potato", "city-state", "electoral college", "gnu", "pony", "chair"];
+var verbs = ['punched', 'considered', 'laughed at', 'selected', 'marginalized', 'drank', "punk'd"];
+var objects = ['my', 'your', 'the'];
+var nouns = ['cat', 'elbow', 'potato', 'city-state', 'electoral college', 'gnu', 'pony', 'chair'];
 var random_message = function(){
-  return "just " + random(verbs) + " " + random(objects) + " " + random(nouns);
-}
+  return 'just ' + random(verbs) + ' ' + random(objects) + ' ' + random(nouns);
+};
 
 // generate random tweets on a random schedule
 var generate_random_tweet = function(){
-  add_tweet({
-    user: random(users),
-    message: random_message(),
-    created_at: new Date()
-  })
-  setTimeout(generate_random_tweet, Math.random() * 1500);
-}
+  var tweet = {};
+  tweet['user'] = randomElement(users);
+  tweet['message'] = random_message();
+  tweet['created_at'] = new Date();
+  add_tweet(tweet);
+  setTimeout(generate_random_tweet, Math['random']() * 1500);
+};
 setTimeout(generate_random_tweet, 0);
 
 // utility function for letting students add "write a tweet" functionality
@@ -48,8 +46,8 @@ var write_tweet = function(message){
   if(!visitor){
     throw new Error('set the global visitor property!');
   }
-  add_tweet({
-    user: visitor,
-    message: message
-  })
-}
+  var tweet = {};
+  tweet['user'] = visitor;
+  tweet['message'] = message;
+  add_tweet(tweet);
+};
--- twitter_ui.js
+++ twitter_ui.js
@@ -1,10 +1,13 @@
 var draw = function(){
-  var body = $("body");
-  body.html("");
-  for(var i = window.streams.home.length - 1; i >= 0; i-- ){
-    var tweet = window.streams.home[i];
-    console.log(tweet);
-    body.append(tweet.user + ": " + tweet.message + "<br>");
+  var $body = $('body');
+  $body['html']('');
+  var index = streams['home']['length'] - 1;
+  while(index >= 0){
+    var tweet = streams['home'][index];
+    console['log']('rendering a tweet:', tweet);
+    var html = tweet['user'] + ': ' + tweet['message'] + '<br>';
+    $body['append'](html);
+    index--;
   }
 }
 setTimeout(draw, 2000);
