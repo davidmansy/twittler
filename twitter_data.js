a8f5c27fbf7768deb2895f49f8e644c667204d11
Marcus Phillips <github.com@marcusphillips.com>
2013-01-31 23:55:33 -0800

    naming in camel case, move twitter_ui to inline script block

--- twitter_data.js
+++ twitter_data.js
@@ -1,6 +1,6 @@
 // set up data structures
 window['streams'] = {};
-streams['home'] = {};
+streams['home'] = [];
 streams['users'] = {};
 streams['users']['shawndrost'] = [];
 streams['users']['sharksforcheap'] = [];
@@ -9,45 +9,45 @@ streams['users']['douglascalhoun'] = [];
 window['users'] = Object['keys'](streams['users']);
 
 // utility function for adding tweets to our data structures
-var add_tweet = function(new_tweet){
-  var username = new_tweet['user'];
-  streams['users'][username]['push'](new_tweet);
-  streams['home']['push'](new_tweet);
+var addTweet = function(newTweet){
+  var username = newTweet['user'];
+  streams['users'][username]['push'](newTweet);
+  streams['home']['push'](newTweet);
 };
 
 // utility function
 var randomElement = function(array){
-  var random_index = Math['floor'](Math['random']() * array['length']);
-  return array[random_index];
+  var randomIndex = Math['floor'](Math['random']() * array['length']);
+  return array[randomIndex];
 };
 
 // random tweet generator
 var verbs = ['punched', 'considered', 'laughed at', 'selected', 'marginalized', 'drank', "punk'd"];
 var objects = ['my', 'your', 'the'];
 var nouns = ['cat', 'elbow', 'potato', 'city-state', 'electoral college', 'gnu', 'pony', 'chair'];
-var random_message = function(){
-  return 'just ' + random(verbs) + ' ' + random(objects) + ' ' + random(nouns);
+var randomMessage = function(){
+  return 'just ' + randomElement(verbs) + ' ' + randomElement(objects) + ' ' + randomElement(nouns);
 };
 
 // generate random tweets on a random schedule
-var generate_random_tweet = function(){
+var generateRandomTweet = function(){
   var tweet = {};
   tweet['user'] = randomElement(users);
-  tweet['message'] = random_message();
+  tweet['message'] = randomMessage();
   tweet['created_at'] = new Date();
-  add_tweet(tweet);
-  setTimeout(generate_random_tweet, Math['random']() * 1500);
+  addTweet(tweet);
+  setTimeout(generateRandomTweet, Math['random']() * 1500);
 };
-setTimeout(generate_random_tweet, 0);
+setTimeout(generateRandomTweet, 0);
 
 // utility function for letting students add "write a tweet" functionality
 // (note: not used by the rest of this file.)
-var write_tweet = function(message){
+var writeTweet = function(message){
   if(!visitor){
     throw new Error('set the global visitor property!');
   }
   var tweet = {};
   tweet['user'] = visitor;
   tweet['message'] = message;
-  add_tweet(tweet);
+  addTweet(tweet);
 };
