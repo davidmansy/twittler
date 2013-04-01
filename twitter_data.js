a0dda742a0a1bdbbc1afae661943418e5cec10df
Marcus Phillips <github.com@marcusphillips.com>
2013-03-31 17:43:47 -0700

    removing bracket notation

--- twitter_data.js
+++ twitter_data.js
@@ -1,40 +1,43 @@
 // set up data structures
-window['streams'] = {};
-streams['home'] = [];
-streams['users'] = {};
-streams['users']['shawndrost'] = [];
-streams['users']['sharksforcheap'] = [];
-streams['users']['mracus'] = [];
-streams['users']['douglascalhoun'] = [];
-window['users'] = Object['keys'](streams['users']);
+window.streams = {};
+streams.home = [];
+streams.users = {};
+streams.users.shawndrost = [];
+streams.users.sharksforcheap = [];
+streams.users.mracus = [];
+streams.users.douglascalhoun = [];
+window.users = Object.keys(streams.users);
 
 // utility function for adding tweets to our data structures
 var addTweet = function(newTweet){
-  var username = newTweet['user'];
-  streams['users'][username]['push'](newTweet);
-  streams['home']['push'](newTweet);
+  var username = newTweet.user;
+  streams.users[username].push(newTweet);
+  streams.home.push(newTweet);
 };
 
 // utility function
 var randomElement = function(array){
-  var randomIndex = Math['floor'](Math['random']() * array['length']);
+  var randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
 };
 
 // random tweet generator
-var verbs = ['punched', 'considered', 'laughed at', 'selected', 'marginalized', 'drank', "punk'd"];
-var objects = ['my', 'your', 'the'];
-var nouns = ['cat', 'elbow', 'potato', 'city-state', 'electoral college', 'gnu', 'pony', 'chair'];
+var opening = ['just', '', '', '', '', 'ask me how i', 'completely', 'nearly', 'productively', 'efficiently', 'last night i', 'the president', 'that wizard', 'a ninja', 'a seedy old man'];
+var verbs = ['drank', 'drunk', 'deployed', 'got', 'developed', 'built', 'invented', 'experienced', 'fought off', 'hardened', 'enjoyed', 'developed', 'consumed', 'debunked', 'drugged', 'doped', 'made', 'wrote', 'saw'];
+var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the', 'the big', 'a new form of'];
+var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'potato', 'money', 'way of life', 'belief system', 'security system', 'bad decision', 'future', 'life', 'pony', 'mind'];
+var tags = ['#techlife', '#burningman', '#sf', 'but only i know how', 'for real', '#sxsw', '#ballin', '#omg', '#yolo', '#magic', '', '', '', ''];
+
 var randomMessage = function(){
-  return 'just ' + randomElement(verbs) + ' ' + randomElement(objects) + ' ' + randomElement(nouns);
+  return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
 };
 
 // generate random tweets on a random schedule
 var generateRandomTweet = function(){
   var tweet = {};
-  tweet['user'] = randomElement(users);
-  tweet['message'] = randomMessage();
-  tweet['created_at'] = new Date();
+  tweet.user = randomElement(users);
+  tweet.message = randomMessage();
+  tweet.created_at = new Date();
   addTweet(tweet);
 };
 
@@ -44,7 +47,7 @@ for(var i = 0; i < 10; i++){
 
 var scheduleNextTweet = function(){
   generateRandomTweet();
-  setTimeout(scheduleNextTweet, Math['random']() * 1500);
+  setTimeout(scheduleNextTweet, Math.random() * 1500);
 };
 scheduleNextTweet();
 
@@ -55,7 +58,7 @@ var writeTweet = function(message){
     throw new Error('set the global visitor property!');
   }
   var tweet = {};
-  tweet['user'] = visitor;
-  tweet['message'] = message;
+  tweet.user = visitor;
+  tweet.message = message;
   addTweet(tweet);
 };
